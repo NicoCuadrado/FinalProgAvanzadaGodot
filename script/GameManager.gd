4878199634dd5b02087cf5b6a3652c497928cce9
NicoCuadrado <cuadradonicolas99@gmail.com>
2025-09-03 21:29:26 -0300

    hud y vidas
    Agregue la funcionalidad de que el personaje tenga 3 vidas y el hud para las vidas

---
+++ script/GameManager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var max_vidas := 3
+var vidas := max_vidas
+
+func reset():
+	vidas = max_vidas
--- script/player.gd
+++ script/player.gd
@@ -2,54 +2,51 @@ class_name Player extends CharacterBody2D
 
 @onready var sprite_animation: AnimatedSprite2D = $AnimatedSprite2D
 @onready var health_component: HealthComponent = $Components/HealthComponent
+@onready var h_box_container: HBoxContainer = $CanvasLayer/HBoxContainer
 
 signal ataque_finalizado 
 
 var velocidad_movimiento := 200
-var fuerza_salto := -300   # valor negativo porque el eje Y crece hacia abajo en Godot
-var gravedad := 600        # para que el personaje caiga
+var fuerza_salto := -300
+var gravedad := 600
 var danio_ataque := 50
 var esta_atacando := false
 var esta_muerto := false
 
 func _ready() -> void:
 	health_component.death.connect(on_dead)
+	update_vidas()
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				ataque()
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		ataque()
 
 func _physics_process(delta: float) -> void:
 	if esta_muerto:
-		return # esto no permite mas inputs si el jugador esta muerto
+		return
 	
-	# aplicar gravedad siempre
+	# Gravedad
 	if !is_on_floor():
 		velocity.y += gravedad * delta
 	else:
-		# si está en el suelo y se presiona espacio → salto
-		if Input.is_action_just_pressed("ui_accept"): # espacio por defecto en Godot
+		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = fuerza_salto
 
 	if !esta_atacando:
 		var direccion_x := 0
-		
-		# movimiento lateral con A y D
-		if Input.is_action_pressed("ui_left"):   # tecla A por defecto
+		if Input.is_action_pressed("ui_left"):
 			direccion_x = -1
-		elif Input.is_action_pressed("ui_right"): # tecla D por defecto
+		elif Input.is_action_pressed("ui_right"):
 			direccion_x = 1
 
 		velocity.x = direccion_x * velocidad_movimiento
-		
-		# animaciones
+
+		# Animaciones
 		if direccion_x != 0:
 			sprite_animation.play("run")
 			sprite_animation.flip_h = direccion_x < 0
 		else:
-			if is_on_floor(): # que solo muestre idle si no está en el aire
+			if is_on_floor():
 				sprite_animation.play("idle")
 
 	move_and_slide()
@@ -57,23 +54,36 @@ func _physics_process(delta: float) -> void:
 func ataque():
 	sprite_animation.play("attack")
 	esta_atacando = true
-# aca termina la animacion de ataque 
+
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if sprite_animation.animation == "attack":
 		esta_atacando = false
 		ataque_finalizado.emit()
 
+func update_vidas():
+	for i in range(GameManager.max_vidas):
+		var vida = h_box_container.get_child(i) as TextureRect
+		if i < GameManager.vidas:
+			vida.texture = load("res://art/Adventurer-1.5/Individual Sprites/adventurer-idle-03.png")
+		else:
+			vida.texture = load("res://art/Adventurer-1.5/Individual Sprites/adventurer-die-06.png")
+
 func on_dead():
 	esta_muerto = true
-	sprite_animation.play("death")
-	await sprite_animation.animation_finished
-	print("game Over")
-	get_tree().paused = true
+	GameManager.vidas -= 1
+	update_vidas()
+
+	if GameManager.vidas > 0:
+		get_tree().reload_current_scene()
+	else:
+		print("Game Over")
+		sprite_animation.play("death")
+		await sprite_animation.animation_finished
+		get_tree().paused = true
 
 func _on_area_attack_body_entered(body: Node2D) -> void:
 	if body is Enemy:
 		body.en_rango_de_ataque_player = true
-
 
 func _on_area_attack_body_exited(body: Node2D) -> void:
 	if body is Enemy:
